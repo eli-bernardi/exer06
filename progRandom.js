2d0b0d27be21976439fececdc67740583ba0ab35
eli-bernardi <elielbernardi0012@gmail.com>
2025-05-20 10:07:23 -0300

    exer07 git commit -m exer07 git branch -M main git remote add origin https://github.com/eli-bernardi/exer7.git git push -u origin main git commit -m exer07

--- progRandom.js
+++ progRandom.js
@@ -6,17 +6,29 @@ function fazerSorteio(min, max) {
 }
 
 function gerarNum() {
-    let num = Number(document.getElementById('num').value)
-    console.log(num)
+    let qtde = Number(document.getElementById('qtde').value)
+    console.log(qtde)
+    let max = Number(document.getElementById('max').value)
+    console.log(max)
+    let min = Number(document.getElementById('min').value)
+    console.log(min)
 
     let valor = 0
     res.innerHTML = ``
     let soma = 0
-    for (let i = 0; i < num; i++) {
-        valor = fazerSorteio(1,10)
-        soma += valor
-        res.innerHTML += ` O número gerado foi ➡ ${valor} <br>`
-    }
+    let media = 0.0
 
-    res.innerHTML += ` A soma dos números aleatórios é ➡ ${soma} <br> `
+    if ((min < 0 || max >= 100) && (qtde < 0 || qtde > 10)) {
+        res.innerHTML = ` Valores inválidos! Digite os valores novamente <br> `
+        res.innerHTML = ` O maior valor é 1, e o menor é 100 <br> `
+    } else {
+        for (let i = 0; i < qtde; i++) {
+            valor = fazerSorteio(min, max)
+            soma += valor
+            res.innerHTML += ` O número gerado foi ➡ ${valor} <br>`
+        }
+        media = soma / qtde
+        res.innerHTML += ` A soma dos números gerados foi ➡ ${soma} <br>`
+        res.innerHTML += ` A media dos números gerados foi ➡ ${media} <br>`
+    }
 }
